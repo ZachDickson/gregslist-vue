00a7db7c8f4b99ed93c686798aade735913bd71b
ZachDickson <z.dickson10@gmail.com>
2020-02-26 14:04:44 -0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Gregslist</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Cars
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Houses</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Jobs</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
---
+++ src/views/CarHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="CarHome">
+    <div class="container-fluid">
+      <div class="row">
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <div class="col-12">cars</div>
+      </div>
+    </div>
+  </div>
+</template>
